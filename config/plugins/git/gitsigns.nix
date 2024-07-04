f2b651fd2351d6cd96e61a3ce9367ed477f72d38
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-07-04 17:52:30 +0200

    Add next/prev hunk keybind

--- config/plugins/git/gitsigns.nix
+++ config/plugins/git/gitsigns.nix
@@ -24,4 +24,19 @@ _: {
       };
     };
   };
+
+  keymaps = [
+    {
+      mode = ["n"];
+      key = "gh";
+      action.__raw = ''function() require('gitsigns').nav_hunk('next') end'';
+      options.desc = "Go to next git change";
+    }
+    {
+      mode = ["n"];
+      key = "gH";
+      action.__raw = ''function() require('gitsigns').nav_hunk('prev') end'';
+      options.desc = "Go to previous git change";
+    }
+  ];
 }
