1505b933e92f80245d507546ddc9122c9f7ca711
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-07-04 17:52:21 +0200

    Add remote flash.nvim keybind

--- config/plugins/utils/flash.nix
+++ config/plugins/utils/flash.nix
@@ -11,9 +11,14 @@ _: {
       action.__raw = "function() require(\"flash\").jump() end";
     }
     {
-      mode = ["n"];
+      mode = ["n" "o"];
       key = "S";
       action.__raw = "function() require(\"flash\").treesitter() end";
+    }
+    {
+      mode = ["o"];
+      key = "r";
+      action.__raw = "function() require(\"flash\").remote() end";
     }
   ];
 }
